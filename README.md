95fe005daaab94bdd0a76fc31e58724fd68eb72a
Cody Stair <ccstair27@gmail.com>
2020-12-18 18:38:37 -0500

    restart
    Co-authored-by: Daniel Lew <danielshlomolew@gmail.com>
    Co-authored-by: Emil Reji <emil315.reji@gmail.com>

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
-# 🏠 [Homepage](https://stagehand-framework.github.io)
 
-![Stagehand logo](images/logos/stagehand_logo-full.png)
+# 🏠 [Homepage](http://bit.ly/2TJckpZ)
+![Crato Banner](https://i.imgur.com/cfeEUFu.png)
 
-## Stagehand: Review Apps For Frontend Developers
+<h1 align="center">Welcome to Crato 👋</h1>
 
-Stagehand makes getting to production as painless as possible by providing a drop-in solution for review apps for modern frontend applications.
+![crato](https://img.shields.io/badge/Crato-%F0%9F%93%A6-1e4470?style=plastic) 
+![version](https://img.shields.io/badge/version-0.7.0-1e4470?style=plastic) 
+![license](https://img.shields.io/github/license/crato-logging/crato?color=1e4470&style=plastic)
 
-# 🏠 [Homepage](https://stagehand-framework.github.io)
+Crato is an open source framework for small applications to easily deploy centralized logging. Crato is built with Node.js, Rsyslog, Apache Kafka, InfluxDB, and AWS S3. This repo contains the code for the Crato website that explains the research and case-study behind the project.
+
+### Crato Client
+
+Crato client is a Bash tool that configures rsyslog to collect system and application logs and send them via TCP to the Crato core infrastructure.
+
+[GitHub Repo](https://github.com/crato-logging/crato-client)
+
+### Crato
+
+Crato is built with Node.js, Rsyslog, Apache Kafka, InfluxDB, and AWS S3. It is the core Crato project that receives, transforms, and stores log data for further analysis.
+
+[GitHub Repo](https://github.com/crato-logging/crato)
+
--- javascripts/application.js
+++ javascripts/application.js
@@ -1,29 +1,78 @@
 /* global document, window, hljs, $ */
 
-document.addEventListener('DOMContentLoaded', () => {
-  const apexLogo = document.querySelector('#apex-logo');
-  const nav = document.querySelector('nav');
-  const navLinks = document.querySelectorAll('#site-navigation a');
-  const main = document.querySelector('main');
-  const header = document.querySelector('header');
-  const ourTeam = document.querySelector('#our-team');
-  const homeLink = document.querySelector('#home-link');
-  const $caseStudyNav = $('#case-study nav');
-  const caseStudyNav = document.querySelector('#case-study nav');
-  const $sideNavLogo = $('#side-nav');
-  const caseStudyLink = document.querySelector('#case-study-link');
-  const ourTeamLink = document.querySelector('#our-team-link');
-  const caseStudyNavUl = document.querySelector('#case-study nav ul');
-  const mobileCaseStudyNavUl = document.querySelector('#case-study-mobile ul');
-  const $toTop = $('#toTop-logo');
+$(function() {
+  const $nextButton = $("#nextButton");
+  const $resetButton = $("#resetButton");
+  const image = document.getElementById("table-state");
 
-  $toTop.on('click', function (e) {
+  $nextButton.on("click", function(e) {
+    e.preventDefault();
+
+    if (image.src.match("state1")) {
+      image.src = "images/diagrams/table_states/state2.png";
+    } else if (image.src.match("state2")) {
+      image.src = "images/diagrams/table_states/state3.png";
+    } else if (image.src.match("state3")) {
+      image.src = "images/diagrams/table_states/state4.png";
+      $nextButton.prop("disabled", true);
+    }
+  });
+
+  $resetButton.on("click", function(e) {
+    e.preventDefault();
+    $nextButton.prop("disabled", false);
+    image.src = "images/diagrams/table_states/state1.png";
+  });
+});
+
+$(function() {
+  const $nextButton2 = $("#nextSystemBtn");
+  const $resetButton2 = $("#resetSystemBtn");
+  const image = document.getElementById("log-collection");
+
+  $nextButton2.on("click", function(e) {
+    e.preventDefault();
+
+    if (image.src.match("log_collection.png")) {
+      image.src = "images/diagrams/log_collection/log_collection_rsyslog.png";
+    } else if (image.src.match("log_collection_rsyslog")) {
+      image.src = "images/diagrams/log_collection/log_collection_syslogng.png";
+    } else if (image.src.match("log_collection_syslogng")) {
+      $nextButton.prop("disabled", true);
+    }
+  });
+
+  $resetButton2.on("click", function(e) {
+    e.preventDefault();
+    $nextButton2.prop("disabled", false);
+    image.src = "images/diagrams/log_collection/log_collection.png";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const cratoLogo = document.querySelector("#crato-logo");
+  const nav = document.querySelector("nav");
+  const navLinks = document.querySelectorAll("#site-navigation a");
+  const main = document.querySelector("main");
+  const header = document.querySelector("header");
+  const ourTeam = document.querySelector("#our-team");
+  const homeLink = document.querySelector("#home-link");
+  const $caseStudyNav = $("#case-study nav");
+  const caseStudyNav = document.querySelector("#case-study nav");
+  const $sideNavLogo = $("#side-nav");
+  const caseStudyLink = document.querySelector("#case-study-link");
+  const ourTeamLink = document.querySelector("#our-team-link");
+  const caseStudyNavUl = document.querySelector("#case-study nav ul");
+  const mobileCaseStudyNavUl = document.querySelector("#case-study-mobile ul");
+  const $toTop = $("#toTop-logo");
+
+  $toTop.on("click", function(e) {
     e.preventDefault();
     $([document.documentElement, document.body]).animate(
       {
-        scrollTop: $('#introduction').offset().top,
+        scrollTop: $("#introduction").offset().top
       },
-      2000,
+      2000
     );
   });
 
@@ -35,131 +84,104 @@ document.addEventListener('DOMContentLoaded', () => {
   const getWindowWidth = () => window.innerWidth;
   const isNarrowScreen = () => getWindowWidth() < 1100;
 
-  const logos = [...document.querySelectorAll('.logo-links img')]
-    .filter((logo) => !/apex/.test(logo.id))
-    .map((logo) => logo.id.split('-')[0]);
+  const logos = [...document.querySelectorAll(".logo-links img")]
+    .filter(logo => !/crato/.test(logo.id))
+    .map(logo => logo.id.split("-")[0]);
 
-  const snakeCaseify = (text) => text.toLowerCase().split(/ +/).join('-').replace(/\./g, '_');
+  const snakeCaseify = text =>
+    text
+      .toLowerCase()
+      .split(" ")
+      .join("-");
 
-  const removeAmpersand = (text) => text.replace('&', '');
-
-  let lastH2Id;
-  const linkableHeaders = [...document.querySelectorAll('#case-study h2, #case-study h3, #our-team h1')].map((el) => {
-    if (el.nodeName === 'H2') {
-      lastH2Id = el.id;
-    }
-
-    return {
-      el,
-      type: el.nodeName,
-      text: el.textContent.replace(/^.*\) /, ''),
-      parentId: lastH2Id
-    }
-  });
-
-  const paddingAllowanceAboveHeading = 30;
+  const h2Text = [...document.querySelectorAll("h2")].map(h2 =>
+    h2.textContent
+      .split(" ")
+      .slice(1)
+      .join(" ")
+  );
 
   const getCaseStudyHeadingPositions = () =>
-    linkableHeaders.reduce((obj, headerObj) => {
-      const selector = `#${snakeCaseify(removeAmpersand(headerObj.text))}`;
-      const header = document.querySelector(selector);
-      const position =
-        getScrollPosition() + header.getBoundingClientRect().top - paddingAllowanceAboveHeading;
-      obj[`${selector}-nav`] = {
-        position,
-        headerData: headerObj
-      };
-
+    h2Text.reduce((obj, h2Str) => {
+      const selector = `#${snakeCaseify(h2Str)}`;
+      const h2 = document.querySelector(selector);
+      //console.log(h2);
+      const position = getScrollPosition() + h2.getBoundingClientRect().top;
+      obj[`${selector}-nav`] = position;
       return obj;
     }, {});
 
   const highlightSection = (li, a) => {
-    li.style.listStyle = 'disc';
+    li.style.listStyle = "disc";
 
-    li.style.color = '#248F99';
-    a.style.color = '#248F99';
+    li.style.color = "#ed6a40";
+    a.style.color = "#ed6a40";
   };
 
   const mobileCaseStudyLinks = [];
 
-  (function () {
-    let lastH2Id;
+  h2Text.forEach(h2TextStr => {
+    const li = document.createElement("li");
+    li.id = snakeCaseify(`${h2TextStr.replace("!", "").toLowerCase()}-nav`);
+    const a = document.createElement("a");
+    a.href = snakeCaseify(`#${h2TextStr.replace("!", "")}`);
+    a.textContent = h2TextStr.toUpperCase();
+    a.className = "case-study-anchor";
 
-    linkableHeaders.forEach((headerObj) => {
-      if (headerObj.type === 'H2') {
-        lastH2Id = headerObj.el.id;
-      } else {
-        headerObj.parentId = lastH2Id;
-      }
+    const li2 = document.createElement("li");
+    li2.id = snakeCaseify(
+      `mobile-${h2TextStr.replace("!", "").toLowerCase()}-nav`
+    );
+    const a2 = document.createElement("a");
+    a2.href = snakeCaseify(`#${h2TextStr.replace("!", "")}`);
+    a2.textContent = h2TextStr.toUpperCase();
 
-      const li = document.createElement('li');
-      li.id = snakeCaseify(`${removeAmpersand(headerObj.text).replace('!', '').toLowerCase()}-nav`);
-      li.dataset['parentHeaderId'] = lastH2Id;
-      li.dataset['tagType'] = headerObj.el.nodeName;
+    li.appendChild(a);
+    caseStudyNavUl.appendChild(li);
 
-      const a = document.createElement('a');
-      a.href = snakeCaseify(`#${removeAmpersand(headerObj.text).replace('!', '')}`);
-      a.textContent = headerObj.text.toUpperCase();
-      a.className = 'case-study-anchor';
-
-      li.appendChild(a);
-      caseStudyNavUl.appendChild(li);
-
-      if (headerObj.type === 'H2') {
-        const li2 = document.createElement('li');
-        li2.id = snakeCaseify(
-          `mobile-${removeAmpersand(headerObj.text).replace('!', '').toLowerCase()}-nav`,
-        );
-        li2.dataset['parentHeaderId'] = lastH2Id;
-        li2.dataset['tagType'] = headerObj.el.nodeName;
-
-        const a2 = document.createElement('a');
-        a2.href = snakeCaseify(`#${removeAmpersand(headerObj.text).replace('!', '')}`);
-        a2.textContent = headerObj.text.toUpperCase();
-
-        mobileCaseStudyLinks.push(a2);
-        li2.appendChild(a2);
-        mobileCaseStudyNavUl.appendChild(li2);
-      }
-
-      headerObj.navEl = li;
-    });
-  }());
+    mobileCaseStudyLinks.push(a2);
+    li2.appendChild(a2);
+    mobileCaseStudyNavUl.appendChild(li2);
+  });
 
   const changeImgSrc = (tag, url) => {
     document.querySelector(`#${tag}`).src = url;
   };
 
   const logoUrls = {
-    githubWhite: './images/logos/github_white.png',
-    githubBlack: './images/logos/github_black.png',
+    githubWhite: "./images/logos/github_white.png",
+    githubBlack: "./images/logos/github_black.png"
   };
 
-  const isOnHeader = (logo) => {
+  const isOnHeader = logo => {
     const position = getScrollPosition();
     const logoSelector = `#${logo}-logo`;
     const logoElement = document.querySelector(logoSelector);
     const logoHeight = logoElement.height;
-    const logoBottom = +window.getComputedStyle(logoElement).bottom.replace('px', '');
+    const logoBottom = +window
+      .getComputedStyle(logoElement)
+      .bottom.replace("px", "");
     const logoOffset = logoHeight + logoBottom;
     return position < logoOffset;
   };
 
-  const isOnTeamSection = (logo) => {
+  const isOnTeamSection = logo => {
     const position = getScrollPosition();
     const ourTeamOffset = ourTeam.getBoundingClientRect().top;
     const ourTeamPosition = position + ourTeamOffset;
     const logoSelector = `#${logo}-logo`;
     const logoElement = document.querySelector(logoSelector);
     const logoHeight = logoElement.height;
-    const logoBottom = +window.getComputedStyle(logoElement).bottom.replace('px', '');
+    const logoBottom = +window
+      .getComputedStyle(logoElement)
+      .bottom.replace("px", "");
     const logoOffset = logoHeight + logoBottom;
     const logoPosition = position + getWindowHeight() - logoOffset;
     return logoPosition > ourTeamPosition;
   };
 
   const changeLogoColors = () => {
-    logos.forEach((logo) => {
+    logos.forEach(logo => {
       const onHeader = isOnHeader(logo);
       const onTeam = isOnTeamSection(logo);
 
@@ -177,43 +199,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const positions = getCaseStudyHeadingPositions();
     const positionValues = Object.values(positions);
     const positionSelectors = Object.keys(positions);
-    const mobileCaseStudyNav = document.querySelector('#case-study-mobile');
+    const mobileCaseStudyNav = document.querySelector("#case-study-mobile");
     const position = getScrollPosition();
 
-    const currentParent = document.querySelector('nav li.active-parent');
-    const currentChildren = document.querySelectorAll('nav li.active-child');
-    let newParentId;
-
     positionValues.forEach((_, i) => {
-      const currentData = positionValues[i].headerData;
-      const li = currentData.navEl;
-      const a = li.getElementsByTagName('a')[0];
-      const currPosition = i > 0 ? positionValues[i].position : 0;
+      const li = document.querySelector(positionSelectors[i]);
+      const a = li.getElementsByTagName("a")[0];
+      const currPosition = i > 0 ? positionValues[i] : 0;
       const nextPositionIdx = i + 1;
-      const nextPosition =
-        (positionValues[nextPositionIdx] &&
-          positionValues[nextPositionIdx].position) || 999999;
-      const windowPositionIsAtLi = position >= currPosition && position < nextPosition;
+      const nextPosition = positionValues[nextPositionIdx] || 999999;
+      const windowPositionIsAtLi =
+        position >= currPosition && position < nextPosition;
 
       if (windowPositionIsAtLi && !mobileCaseStudyNav.contains(li)) {
         highlightSection(li, a);
-
-        newParentId = currentData.parentId;
       } else {
-        if (li.getAttribute('style')) li.removeAttribute('style');
-        if (a.getAttribute('style')) a.removeAttribute('style');
+        if (li.getAttribute("style")) li.removeAttribute("style");
+        if (a.getAttribute("style")) a.removeAttribute("style");
       }
-    });
-
-    if (currentParent) {
-      currentParent.classList.remove('active-parent');
-    }
-
-    currentChildren.forEach(child => child.classList.remove('active-child'));
-
-    newActive = document.querySelectorAll(`[data-parent-header-id="${newParentId}"]`)
-    newActive.forEach(el => {
-      el.classList.add('active-child');
     });
   };
 
@@ -222,11 +225,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const ourTeamPosition = position + ourTeam.getBoundingClientRect().top;
     const mainPosition = position + main.getBoundingClientRect().top;
     const withinCaseStudy =
-      position >= mainPosition && position < ourTeamPosition - getWindowHeight();
+      position >= mainPosition &&
+      position < ourTeamPosition - getWindowHeight();
 
-    if (getWindowHeight() < 650 || getWindowWidth() < 1100) {
-      $sideNavLogo.stop(true, true).css('display', 'none');
-      $caseStudyNav.stop(true, true).css('display', 'none');
+    if (getWindowHeight() < 500 || getWindowWidth() < 1100) {
+      $sideNavLogo.stop(true, true).css("display", "none");
+      $caseStudyNav.stop(true, true).css("display", "none");
     } else if (withinCaseStudy) {
       $sideNavLogo.fadeIn(800);
       $caseStudyNav.fadeIn(800);
@@ -234,48 +238,50 @@ document.addEventListener('DOMContentLoaded', () => {
 
       handleCaseStudyNavStyles();
     } else {
-      $sideNavLogo.stop(true, true).css('display', 'none');
-      $caseStudyNav.stop(true, true).css('display', 'none');
-      $toTop.stop(true, true).css('display', 'none');
+      $sideNavLogo.stop(true, true).css("display", "none");
+      $caseStudyNav.stop(true, true).css("display", "none");
+      $toTop.stop(true, true).css("display", "none");
     }
 
     if (getWindowHeight() < 500) {
-      $toTop.stop(true, true).css('display', 'none');
+      $toTop.stop(true, true).css("display", "none");
     } else if (withinCaseStudy) {
       $toTop.fadeIn(800);
     } else {
-      $toTop.stop(true, true).css('display', 'none');
+      $toTop.stop(true, true).css("display", "none");
     }
   };
 
   const styleNavColors = (bgColor, textColor, hoverColor) => {
     nav.style.backgroundColor = bgColor;
-    const links = Array.prototype.slice.call(navLinks).concat(mobileCaseStudyLinks);
-    links.forEach((link) => {
+    const links = Array.prototype.slice
+      .call(navLinks)
+      .concat(mobileCaseStudyLinks);
+    links.forEach(link => {
       link.style.color = textColor;
 
-      link.addEventListener('mouseenter', () => {
+      link.addEventListener("mouseenter", () => {
         link.style.color = hoverColor;
       });
 
-      link.addEventListener('mouseleave', () => {
+      link.addEventListener("mouseleave", () => {
         link.style.color = textColor;
       });
     });
   };
 
   const handleNavColors = () => {
-    const onHeader = isOnHeader('apex');
-    const onTeam = isOnTeamSection('apex');
+    const onHeader = isOnHeader("crato");
+    const onTeam = isOnTeamSection("crato");
     const onMain = !(onHeader || onTeam);
     const isWideScreen = !isNarrowScreen();
 
     if (isWideScreen && !onMain && topNavVisible) {
-      styleNavColors('#f7f7f7', '#65c8d0', '#248F99');
-      changeImgSrc('apex-logo', 'images/logos/crato-logo.png'); // black
+      styleNavColors("#1e4470", "#edf5e1", "#ed6a40");
+      changeImgSrc("crato-logo", "images/logos/crato-logo.png"); // black
     } else {
-      styleNavColors('#f7f7f7', '#65c8d0', '#248F99');
-      changeImgSrc('apex-logo', 'images/logos/crato-logo.png');
+      styleNavColors("#1e4470", "#edf5e1", "#ed6a40");
+      changeImgSrc("crato-logo", "images/logos/crato-logo.png");
     }
   };
 
@@ -287,27 +293,27 @@ document.addEventListener('DOMContentLoaded', () => {
     handleNavColors();
     scrollPosition = position;
 
-    if (narrowScreen) document.body.style.backgroundColor = '#282828';
-    $(nav).slideDown('fast');
+    if (narrowScreen) document.body.style.backgroundColor = "#282828";
+    $(nav).slideDown("fast");
   };
 
   const showSite = () => {
     const siteElements = [header, main, ourTeam, document.body];
     // remove "display = 'none'" set when small nav was displayed
-    siteElements.forEach((el) => el.removeAttribute('style'));
+    siteElements.forEach(el => el.removeAttribute("style"));
   };
 
   const hideSite = () => {
-    header.style.display = 'none';
-    main.style.display = 'none';
-    ourTeam.style.display = 'none';
+    header.style.display = "none";
+    main.style.display = "none";
+    ourTeam.style.display = "none";
   };
 
   const hideNav = () => {
     smallNavVisible = false;
     topNavVisible = false;
     handleNavColors();
-    $(nav).slideUp('fast');
+    $(nav).slideUp("fast");
     showSite();
   };
 
@@ -330,16 +336,16 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
-  apexLogo.addEventListener('click', handleNavDisplay);
-  main.addEventListener('mouseenter', hideNav);
-  ourTeam.addEventListener('mouseenter', hideNav);
-  header.addEventListener('mouseenter', hideNav);
-  homeLink.addEventListener('click', hideNav);
-  caseStudyLink.addEventListener('click', hideNav);
-  mobileCaseStudyLinks.forEach((link) => link.addEventListener('click', hideNav));
-  ourTeamLink.addEventListener('click', hideNav);
+  cratoLogo.addEventListener("click", handleNavDisplay);
+  main.addEventListener("mouseenter", hideNav);
+  ourTeam.addEventListener("mouseenter", hideNav);
+  header.addEventListener("mouseenter", hideNav);
+  homeLink.addEventListener("click", hideNav);
+  caseStudyLink.addEventListener("click", hideNav);
+  mobileCaseStudyLinks.forEach(link => link.addEventListener("click", hideNav));
+  ourTeamLink.addEventListener("click", hideNav);
 
-  document.addEventListener('scroll', () => {
+  document.addEventListener("scroll", () => {
     if (!smallNavVisible) {
       changeLogoColors();
       handleCaseStudyNav();
@@ -347,7 +353,7 @@ document.addEventListener('DOMContentLoaded', () => {
     handleNavColors();
   });
 
-  window.addEventListener('resize', () => {
+  window.addEventListener("resize", () => {
     handleCaseStudyNav();
     handleNavColors();
     if (!isNarrowScreen() && smallNavVisible) {
@@ -357,12 +363,12 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  caseStudyNav.addEventListener('click', (e) => {
-    if (e.target.tagName === 'A') {
+  caseStudyNav.addEventListener("click", e => {
+    if (e.target.tagName === "A") {
       e.preventDefault();
       const positions = getCaseStudyHeadingPositions();
-      const positionKey = `#${e.target.href.split('#')[1]}-nav`;
-      const newScrollPosition = positions[positionKey].position;
+      const positionKey = `#${e.target.href.split("#")[1]}-nav`;
+      const newScrollPosition = positions[positionKey];
       window.scrollTo(0, newScrollPosition + 5);
     }
   });
