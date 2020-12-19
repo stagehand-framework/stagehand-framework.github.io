c21af43a64218e61a05d06bb401e7327594b9897
Cody Stair <ccstair27@gmail.com>
2020-12-19 11:06:19 -0500

    2nd restart
    Co-authored-by: Daniel Lew <danielshlomolew@gmail.com>
    Co-authored-by: Emil Reji <emil315.reji@gmail.com>

--- README.md
+++ README.md
@@ -1,24 +1,10 @@
+<p align="center">
+  <img src="images/logos/river_logo-readme.png" />
+</p>
 
-# 🏠 [Homepage](http://bit.ly/2TJckpZ)
-![Crato Banner](https://i.imgur.com/cfeEUFu.png)
+<h1 align="center">River: A drop-in real-time service for web applications</h1>
+<h2 align="center">Cloud-native, easily deployed, and ready to scale with your needs</h2>
 
-<h1 align="center">Welcome to Crato 👋</h1>
+For the application developer, River abstracts away the complexity of setting up a resilient infrastructure and removes the hassle of managing WebSocket connections. It allows backend services to publish events and makes these events available to web clients in real-time. River is a great fit for existing applications looking for a way to easily add real-time functionality.
 
-![crato](https://img.shields.io/badge/Crato-%F0%9F%93%A6-1e4470?style=plastic) 
-![version](https://img.shields.io/badge/version-0.7.0-1e4470?style=plastic) 
-![license](https://img.shields.io/github/license/crato-logging/crato?color=1e4470&style=plastic)
-
-Crato is an open source framework for small applications to easily deploy centralized logging. Crato is built with Node.js, Rsyslog, Apache Kafka, InfluxDB, and AWS S3. This repo contains the code for the Crato website that explains the research and case-study behind the project.
-
-### Crato Client
-
-Crato client is a Bash tool that configures rsyslog to collect system and application logs and send them via TCP to the Crato core infrastructure.
-
-[GitHub Repo](https://github.com/crato-logging/crato-client)
-
-### Crato
-
-Crato is built with Node.js, Rsyslog, Apache Kafka, InfluxDB, and AWS S3. It is the core Crato project that receives, transforms, and stores log data for further analysis.
-
-[GitHub Repo](https://github.com/crato-logging/crato)
-
+To learn more, please read our [case study](http://river-live.github.io) or feel free to drop us a line!
--- javascripts/application.js
+++ javascripts/application.js
@@ -1,56 +1,7 @@
 /* global document, window, hljs, $ */
 
-$(function() {
-  const $nextButton = $("#nextButton");
-  const $resetButton = $("#resetButton");
-  const image = document.getElementById("table-state");
-
-  $nextButton.on("click", function(e) {
-    e.preventDefault();
-
-    if (image.src.match("state1")) {
-      image.src = "images/diagrams/table_states/state2.png";
-    } else if (image.src.match("state2")) {
-      image.src = "images/diagrams/table_states/state3.png";
-    } else if (image.src.match("state3")) {
-      image.src = "images/diagrams/table_states/state4.png";
-      $nextButton.prop("disabled", true);
-    }
-  });
-
-  $resetButton.on("click", function(e) {
-    e.preventDefault();
-    $nextButton.prop("disabled", false);
-    image.src = "images/diagrams/table_states/state1.png";
-  });
-});
-
-$(function() {
-  const $nextButton2 = $("#nextSystemBtn");
-  const $resetButton2 = $("#resetSystemBtn");
-  const image = document.getElementById("log-collection");
-
-  $nextButton2.on("click", function(e) {
-    e.preventDefault();
-
-    if (image.src.match("log_collection.png")) {
-      image.src = "images/diagrams/log_collection/log_collection_rsyslog.png";
-    } else if (image.src.match("log_collection_rsyslog")) {
-      image.src = "images/diagrams/log_collection/log_collection_syslogng.png";
-    } else if (image.src.match("log_collection_syslogng")) {
-      $nextButton.prop("disabled", true);
-    }
-  });
-
-  $resetButton2.on("click", function(e) {
-    e.preventDefault();
-    $nextButton2.prop("disabled", false);
-    image.src = "images/diagrams/log_collection/log_collection.png";
-  });
-});
-
 document.addEventListener("DOMContentLoaded", () => {
-  const cratoLogo = document.querySelector("#crato-logo");
+  const riverLogo = document.querySelector("#river-logo");
   const nav = document.querySelector("nav");
   const navLinks = document.querySelectorAll("#site-navigation a");
   const main = document.querySelector("main");
@@ -64,13 +15,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const ourTeamLink = document.querySelector("#our-team-link");
   const caseStudyNavUl = document.querySelector("#case-study nav ul");
   const mobileCaseStudyNavUl = document.querySelector("#case-study-mobile ul");
+  const $githubLogo = $("#github-logo");
   const $toTop = $("#toTop-logo");
 
-  $toTop.on("click", function(e) {
+  $toTop.on("click", function (e) {
     e.preventDefault();
     $([document.documentElement, document.body]).animate(
       {
-        scrollTop: $("#introduction").offset().top
+        scrollTop: $("#what-is-river").offset().top,
       },
       2000
     );
@@ -85,28 +37,28 @@ document.addEventListener("DOMContentLoaded", () => {
   const isNarrowScreen = () => getWindowWidth() < 1100;
 
   const logos = [...document.querySelectorAll(".logo-links img")]
-    .filter(logo => !/crato/.test(logo.id))
-    .map(logo => logo.id.split("-")[0]);
+    .filter((logo) => !/river/.test(logo.id))
+    .map((logo) => logo.id.split("-")[0]);
 
-  const snakeCaseify = text =>
-    text
-      .toLowerCase()
-      .split(" ")
-      .join("-");
+  const snakeCaseify = (text) => text.toLowerCase().split(/ +/).join("-");
 
-  const h2Text = [...document.querySelectorAll("h2")].map(h2 =>
-    h2.textContent
-      .split(" ")
-      .slice(1)
-      .join(" ")
+  const removeAmpersand = (text) => text.replace("&", "");
+  const removeQuestionMark = (text) => text.replace("?", "");
+
+  const h2Text = [...document.querySelectorAll("h2")].map((h2) =>
+    removeQuestionMark(h2.textContent).split(" ").slice(1).join(" ")
   );
+
+  const paddingAllowanceAboveHeading = 30;
 
   const getCaseStudyHeadingPositions = () =>
     h2Text.reduce((obj, h2Str) => {
-      const selector = `#${snakeCaseify(h2Str)}`;
+      const selector = `#${snakeCaseify(removeAmpersand(h2Str))}`;
       const h2 = document.querySelector(selector);
-      //console.log(h2);
-      const position = getScrollPosition() + h2.getBoundingClientRect().top;
+      const position =
+        getScrollPosition() +
+        h2.getBoundingClientRect().top -
+        paddingAllowanceAboveHeading;
       obj[`${selector}-nav`] = position;
       return obj;
     }, {});
@@ -114,26 +66,28 @@ document.addEventListener("DOMContentLoaded", () => {
   const highlightSection = (li, a) => {
     li.style.listStyle = "disc";
 
-    li.style.color = "#ed6a40";
-    a.style.color = "#ed6a40";
+    li.style.color = "#0779e4";
+    a.style.color = "#0779e4";
   };
 
   const mobileCaseStudyLinks = [];
 
-  h2Text.forEach(h2TextStr => {
+  h2Text.forEach((h2TextStr) => {
     const li = document.createElement("li");
-    li.id = snakeCaseify(`${h2TextStr.replace("!", "").toLowerCase()}-nav`);
+    li.id = snakeCaseify(
+      `${removeAmpersand(h2TextStr).replace("!", "").toLowerCase()}-nav`
+    );
     const a = document.createElement("a");
-    a.href = snakeCaseify(`#${h2TextStr.replace("!", "")}`);
+    a.href = snakeCaseify(`#${removeAmpersand(h2TextStr).replace("!", "")}`);
     a.textContent = h2TextStr.toUpperCase();
     a.className = "case-study-anchor";
 
     const li2 = document.createElement("li");
     li2.id = snakeCaseify(
-      `mobile-${h2TextStr.replace("!", "").toLowerCase()}-nav`
+      `mobile-${removeAmpersand(h2TextStr).replace("!", "").toLowerCase()}-nav`
     );
     const a2 = document.createElement("a");
-    a2.href = snakeCaseify(`#${h2TextStr.replace("!", "")}`);
+    a2.href = snakeCaseify(`#${removeAmpersand(h2TextStr).replace("!", "")}`);
     a2.textContent = h2TextStr.toUpperCase();
 
     li.appendChild(a);
@@ -149,11 +103,12 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   const logoUrls = {
-    githubWhite: "./images/logos/github_white.png",
-    githubBlack: "./images/logos/github_black.png"
+    githubWhite: "./images/logos/river_github-white.png",
+    githubBlack: "./images/logos/river_github-black.png",
+    githubBlue: "./images/logos/river_github-blue.png",
   };
 
-  const isOnHeader = logo => {
+  const isOnHeader = (logo) => {
     const position = getScrollPosition();
     const logoSelector = `#${logo}-logo`;
     const logoElement = document.querySelector(logoSelector);
@@ -165,7 +120,7 @@ document.addEventListener("DOMContentLoaded", () => {
     return position < logoOffset;
   };
 
-  const isOnTeamSection = logo => {
+  const isOnTeamSection = (logo) => {
     const position = getScrollPosition();
     const ourTeamOffset = ourTeam.getBoundingClientRect().top;
     const ourTeamPosition = position + ourTeamOffset;
@@ -181,7 +136,7 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   const changeLogoColors = () => {
-    logos.forEach(logo => {
+    logos.forEach((logo) => {
       const onHeader = isOnHeader(logo);
       const onTeam = isOnTeamSection(logo);
 
@@ -257,7 +212,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const links = Array.prototype.slice
       .call(navLinks)
       .concat(mobileCaseStudyLinks);
-    links.forEach(link => {
+    links.forEach((link) => {
       link.style.color = textColor;
 
       link.addEventListener("mouseenter", () => {
@@ -271,17 +226,17 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   const handleNavColors = () => {
-    const onHeader = isOnHeader("crato");
-    const onTeam = isOnTeamSection("crato");
+    const onHeader = isOnHeader("river");
+    const onTeam = isOnTeamSection("river");
     const onMain = !(onHeader || onTeam);
     const isWideScreen = !isNarrowScreen();
 
     if (isWideScreen && !onMain && topNavVisible) {
-      styleNavColors("#1e4470", "#edf5e1", "#ed6a40");
-      changeImgSrc("crato-logo", "images/logos/crato-logo.png"); // black
+      styleNavColors("#f7f7f7", "#0779e4", "#28c9ae");
+      changeImgSrc("river-logo", "images/logos/river_logo-full.png"); // black
     } else {
-      styleNavColors("#1e4470", "#edf5e1", "#ed6a40");
-      changeImgSrc("crato-logo", "images/logos/crato-logo.png");
+      styleNavColors("#f7f7f7", "#0779e4", "#28c9ae");
+      changeImgSrc("river-logo", "images/logos/river_logo-full.png");
     }
   };
 
@@ -300,7 +255,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const showSite = () => {
     const siteElements = [header, main, ourTeam, document.body];
     // remove "display = 'none'" set when small nav was displayed
-    siteElements.forEach(el => el.removeAttribute("style"));
+    siteElements.forEach((el) => el.removeAttribute("style"));
   };
 
   const hideSite = () => {
@@ -336,13 +291,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  cratoLogo.addEventListener("click", handleNavDisplay);
+  riverLogo.addEventListener("click", handleNavDisplay);
   main.addEventListener("mouseenter", hideNav);
   ourTeam.addEventListener("mouseenter", hideNav);
   header.addEventListener("mouseenter", hideNav);
   homeLink.addEventListener("click", hideNav);
   caseStudyLink.addEventListener("click", hideNav);
-  mobileCaseStudyLinks.forEach(link => link.addEventListener("click", hideNav));
+  mobileCaseStudyLinks.forEach((link) =>
+    link.addEventListener("click", hideNav)
+  );
   ourTeamLink.addEventListener("click", hideNav);
 
   document.addEventListener("scroll", () => {
@@ -363,7 +320,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  caseStudyNav.addEventListener("click", e => {
+  caseStudyNav.addEventListener("click", (e) => {
     if (e.target.tagName === "A") {
       e.preventDefault();
       const positions = getCaseStudyHeadingPositions();
@@ -371,6 +328,14 @@ document.addEventListener("DOMContentLoaded", () => {
       const newScrollPosition = positions[positionKey];
       window.scrollTo(0, newScrollPosition + 5);
     }
+  });
+
+  $githubLogo.on("mouseover touchstart", function () {
+    $githubLogo.prop("src", logoUrls.githubBlue);
+  });
+
+  $githubLogo.on("mouseout touchend", function () {
+    $githubLogo.prop("src", logoUrls.githubBlack);
   });
 
   hljs.initHighlightingOnLoad();
